93b0d1dee2ed34615429a9b7e07cb362eee2bb17
kaushal_pareek <kaushal.pareek@preqin.com>
2020-11-16 13:19:42 +0530

    Added sql server docker and created CQRS design for ordering.api

---
+++ src/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    }
+}
