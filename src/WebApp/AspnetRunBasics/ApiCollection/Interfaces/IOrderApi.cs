60b1daf765a63c8a4c3dcc5cf5bc4efe9df180bc
kaushal_pareek <kaushal.pareek@preqin.com>
2020-11-17 14:05:30 +0530

    Developing API and modifying razor pager to use new models

---
+++ src/WebApp/AspnetRunBasics/ApiCollection/Interfaces/IOrderApi.cs
@@ -0,0 +1,11 @@
+﻿using AspnetRunBasics.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection.Interfaces
+{
+    public interface IOrderApi
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
